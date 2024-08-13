b0e66e3217a343ff69560de5759234c8e51dbb01
pauorfsantos17 <pauorfsantos17@gmail.com>
2024-08-13 10:08:03 -0300

    feat: finally challenge booleans

--- desafios/02-booleanos.ts
+++ desafios/02-booleanos.ts
@@ -1,6 +1,6 @@
 // Boleanos
 
-const user = {
+const client = {
   name: 'Diego Fernandes',
   height: 190,
   hasTicket: true,
@@ -10,22 +10,22 @@ const necessaryHeight = 130
 
 const currentHour = new Date().getHours()
 
-const parkOpen = currentHour > 9 && currentHour < 18
+const isParkOpen = currentHour > 9 && currentHour < 18
 
-if (!parkOpen) {
+if (!isParkOpen) {
   throw new Error('O parque está fechado!')
 }
 
-const ticket = user.hasTicket
+const isClientHasTicker = client.hasTicket
 
-if (!ticket) {
+if (!isClientHasTicker) {
   throw new Error('O Diego não possui um bilhete para entrar no parque!')
 }
 
-const enterToy = user.height > necessaryHeight
+const hasNecessaryHeight = client.height > necessaryHeight
 
-if (!enterToy) {
-  throw new Error('O Diego não pode entrar no brinquedo!')
+if (!hasNecessaryHeight) {
+  throw new Error('O Diego não pode entrar no brinquedo porque tem menos de 130cm!')
 } 
 
 console.log('O Diego se divertiu muito! :)')
